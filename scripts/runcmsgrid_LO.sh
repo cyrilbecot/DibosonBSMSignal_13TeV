87cb8cff7682d0bb1cb05d4059693067742873f5
Shin-Shan Eiko Yu <syu@Shin-Shans-MacBook-Air.local>
2014-12-22 20:41:50 +0100

    script for producing LHE files with weights

---
+++ scripts/runcmsgrid_LO.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd /afs/cern.ch/work/s/syu/public/CMSSW_7_2_3/src
+export SCRAM_ARCH=slc6_amd64_gcc481; eval `scramv1 runtime -sh`
+cd -
+
+#make sure lhapdf points to local cmssw installation area
+LHAPDFCONFIG=`echo "$LHAPDF_DATA_PATH/../../bin/lhapdf-config"`
+
+#if lhapdf6 external is available then above points to lhapdf5 and needs to be overridden
+LHAPDF6TOOLFILE=$CMSSW_BASE/config/toolbox/$SCRAM_ARCH/tools/available/lhapdf6.xml
+if [ -e $LHAPDF6TOOLFILE ]; then
+  LHAPDFCONFIG=`cat $LHAPDF6TOOLFILE | grep "<environment name=\"LHAPDF6_BASE\"" | cut -d \" -f 4`/bin/lhapdf-config
+fi
+
+#make sure env variable for pdfsets points to the right place
+export LHAPDF_DATA_PATH=`$LHAPDFCONFIG --datadir`
+
+
+
+outfile="final_"`echo "${1##*/}"`
+echo "Producing weights and saved in "$outfile
+./mgbasedir/SysCalc/sys_calc $1 syscalc_card.dat $outfile
