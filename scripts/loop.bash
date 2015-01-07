6317f6695a6359ed2fdce32c4bce4455d220a69f
Shin-Shan Yu <syu@ncu200.cern.ch>
2015-01-07 10:53:18 +0100

    add more one argument

--- scripts/loop.bash
+++ scripts/loop.bash
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 scriptname=`basename $0`
-EXPECTED_ARGS=3
+EXPECTED_ARGS=4
 
 if [ $# -ne $EXPECTED_ARGS ]
 then
-    echo "Usage: $scriptname currentDirectory processName massinputFile"
-    echo "Example: ./$scriptname $PWD Zprime_Zh_Zlephbb $PWD/mass_points"
+    echo "Usage: $scriptname generationScript currentDirectory processName massinputFile"
+    echo "Example: ./$scriptname Radion_hh_generation.sh $PWD Radion_hh_hbbhbb $PWD/mass_points"
     exit 1
 fi
 
 iteration=0
-lastfile=`cat $3 | wc -l`
+lastfile=`cat $4 | wc -l`
 echo "There are "$lastfile" mass points"
 
 while [ $iteration -lt $lastfile ]; 
 do
   iteration=$(( iteration + 1 ))
-  mass=(`head -n $iteration $3  | tail -1`)
+  mass=(`head -n $iteration $4  | tail -1`)
   echo "Submitting jobs for X mass = "$mass" GeV"
-  bsub -q1nw $1/Radion_hh_generation.sh $1 $2 $mass
+  bsub -q1nw $2/$1 $2 $3 $mass
 done
