86e836eb8636db80cb2030d047a6eb07146dcb27
Shin-Shan Yu <syu@ncu200.cern.ch>
2015-01-13 13:11:39 +0100

    fix so that all directories are run

--- scripts/runAll.sh
+++ scripts/runAll.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+ls $1 >& filenames.txt
+
 SUFF=lhe
 suff=root
 
-for i in $(ls $1/*.$SUFF)
+while read filename
+do
+    echo "Printing: $filename"
+for i in $(ls $1/$filename/*.$SUFF)
 do
   temp=${i%.$SUFF}.$suff
   outputfile=/data4/syu/13TeV_Diboson`echo "${temp##*Diboson}"`
+#  echo $i 
+#  echo $outputfile
   cmsRun dumpLHE_cfg.py inputFiles="file:"$i outputFile=$outputfile
-done #
+done 
 
+done < filenames.txt
