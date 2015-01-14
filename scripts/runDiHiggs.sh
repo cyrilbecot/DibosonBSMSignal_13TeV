b35c10f2ebbc07093d54c57b61f2c787de18fb1b
Shin-Shan Yu <syu@ncu200.cern.ch>
2015-01-14 16:48:24 +0100

    fixed

--- scripts/runDiHiggs.sh
+++ scripts/runDiHiggs.sh
@@ -7,7 +7,7 @@ for i in $(ls $1)
 do
   temp=${i%.$SUFF}.$suff
   outputfile=/data4/syu/13TeV_Diboson`echo "${temp##*Diboson}"`
-#  echo $i 
+#  echo $1/$i 
 #  echo $outputfile
-  cmsRun dumpLHE_cfg.py inputFiles="file:"$i outputFile=$outputfile
+  cmsRun dumpLHE_cfg.py inputFiles=file:$1/$i outputFile=$outputfile
 done 
