fc51c0038b68fb26b4eea9c70a735d7a3c7d3ea4
Shin-Shan <syu@cern.ch>
2015-04-02 08:33:32 +0200

    updated

--- gridpack_folder/mc_request/replace.bash
+++ gridpack_folder/mc_request/replace.bash
@@ -9,6 +9,6 @@ fi
 location="\/cvmfs\/cms.cern.ch\/phys_generator\/gridpacks\/slc6_amd64_gcc481\/13TeV\/madgraph\/V5_2.2.2\/exo_diboson\/Spin-1\/${process}\/narrow\/v2\/"${type}"_tarball.tar.xz"
 echo $location
 filename=${type}_cff.py
-echo $filename
+echo $filename >> file.txt
 
 sed -e 's/PROCESS/'$process'/g' -e 's/TYPE/'$type'/g' -e 's/LOCATION/'$location'/g' template_cff.py > $process/$filename
