ecf3c011deb9804f5298f3f2966531c55eb4730c
Shin-Shan <syu@cern.ch>
2015-04-03 18:32:52 +0200

    python files for spin-2

--- gridpack_folder/mc_request/replace.bash
+++ gridpack_folder/mc_request/replace.bash
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 type=$1
+spin=$2
 process=${type%%_narrow*}
 echo $process
 if [[ ! -e $process ]]; then 
     mkdir $process
 fi
-location="\/cvmfs\/cms.cern.ch\/phys_generator\/gridpacks\/slc6_amd64_gcc481\/13TeV\/madgraph\/V5_2.2.2\/exo_diboson\/Spin-1\/${process}\/narrow\/v2\/"${type}"_tarball.tar.xz"
+location="\/cvmfs\/cms.cern.ch\/phys_generator\/gridpacks\/slc6_amd64_gcc481\/13TeV\/madgraph\/V5_2.2.2\/exo_diboson\/Spin-${spin}\/${process}\/narrow\/v2\/"${type}"_tarball.tar.xz"
 echo $location
 filename=${type}_cff.py
 echo $filename >> file.txt
 
-sed -e 's/PROCESS/'$process'/g' -e 's/TYPE/'$type'/g' -e 's/LOCATION/'$location'/g' template_cff.py > $process/$filename
+sed -e 's/SPIN/'$spin'/g' -e 's/PROCESS/'$process'/g' -e 's/TYPE/'$type'/g' -e 's/LOCATION/'$location'/g' template_cff.py > $process/$filename
--- gridpack_folder/mc_request/template_cff.py
+++ gridpack_folder/mc_request/template_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 # link to cards:
-# https://github.com/cms-sw/genproductions/tree/master/bin/MadGraph5_aMCatNLO/cards/production/13TeV/exo_diboson/Spin-1/PROCESS/TYPE
+# https://github.com/cms-sw/genproductions/tree/master/bin/MadGraph5_aMCatNLO/cards/production/13TeV/exo_diboson/Spin-SPIN/PROCESS/TYPE
 
 
 externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
