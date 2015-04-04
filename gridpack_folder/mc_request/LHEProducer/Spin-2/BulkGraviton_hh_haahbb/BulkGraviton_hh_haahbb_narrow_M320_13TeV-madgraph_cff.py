fa11abe47783176ce6636a1cc2f69f95af8a0e3d
Shin-Shan <syu@cern.ch>
2015-04-04 08:03:01 +0200

    add LHE producer python files for Spin-2 dihiggs modes

---
+++ gridpack_folder/mc_request/LHEProducer/Spin-2/BulkGraviton_hh_haahbb/BulkGraviton_hh_haahbb_narrow_M320_13TeV-madgraph_cff.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+# link to cards:
+# https://github.com/cms-sw/genproductions/tree/master/bin/MadGraph5_aMCatNLO/cards/production/13TeV/exo_diboson/Spin-2/BulkGraviton_hh_haahbb/BulkGraviton_hh_haahbb_narrow_M320
+
+
+externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.2.2/exo_diboson/Spin-2/BulkGraviton_hh_haahbb/narrow/v1/BulkGraviton_hh_haahbb_narrow_M320_tarball.tar.xz'),
+    nEvents = cms.untracked.uint32(5000),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
+)
