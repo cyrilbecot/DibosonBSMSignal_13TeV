38a9513fb5a62f1fa36b1a08c60affa9061d9fa5
syuvivida <syu@phy.ncu.edu.tw>
2015-05-30 22:39:42 +0800

    add configuration files for BulkGraviton_hh_hVVhbb_fullLep, BulkGraviton_hh_haahbb, BulkGraviton_hh_htatahbb

--- gridpack_folder/mc_request/replace.bash
+++ gridpack_folder/mc_request/replace.bash
@@ -7,7 +7,7 @@ echo $process
 if [[ ! -e $process ]]; then 
     mkdir $process
 fi
-location="\/cvmfs\/cms.cern.ch\/phys_generator\/gridpacks\/slc6_amd64_gcc481\/13TeV\/madgraph\/V5_2.2.2\/exo_diboson\/Spin-${spin}\/${process}\/narrow\/v2\/"${type}"_tarball.tar.xz"
+location="\/cvmfs\/cms.cern.ch\/phys_generator\/gridpacks\/slc6_amd64_gcc481\/13TeV\/madgraph\/V5_2.2.2\/exo_diboson\/Spin-${spin}\/${process}\/narrow\/v3\/"${type}"_tarball.tar.xz"
 #location="\/cvmfs\/cms.cern.ch\/phys_generator\/gridpacks\/slc6_amd64_gcc481\/13TeV\/madgraph\/V5_2.2.2\/exo_diboson\/Spin-${spin}\/${process}\/narrow\/v1\/"${type}"_tarball.tar.xz"
 echo $location
 filename=${type}_13TeV-madgraph_cff.py
