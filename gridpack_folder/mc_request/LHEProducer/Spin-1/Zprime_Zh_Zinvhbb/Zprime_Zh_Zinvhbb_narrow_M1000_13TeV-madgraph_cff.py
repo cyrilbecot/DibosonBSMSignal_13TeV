1f0652e757f90bfa99dc451922ea8a85ac12d8ee
syuvivida <syu@phy.ncu.edu.tw>
2015-07-11 11:56:06 +0200

    replace v1 with v2

--- gridpack_folder/mc_request/LHEProducer/Spin-1/Zprime_Zh_Zinvhbb/Zprime_Zh_Zinvhbb_narrow_M1000_13TeV-madgraph_cff.py
+++ gridpack_folder/mc_request/LHEProducer/Spin-1/Zprime_Zh_Zinvhbb/Zprime_Zh_Zinvhbb_narrow_M1000_13TeV-madgraph_cff.py
@@ -5,7 +5,7 @@ import FWCore.ParameterSet.Config as cms
 
 
 externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
-    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.2.2/exo_diboson/Spin-1/Zprime_Zh_Zinvhbb/narrow/v1/Zprime_Zh_Zinvhbb_narrow_M1000_tarball.tar.xz'),
+    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.2.2/exo_diboson/Spin-1/Zprime_Zh_Zinvhbb/narrow/v2/Zprime_Zh_Zinvhbb_narrow_M1000_tarball.tar.xz'),
     nEvents = cms.untracked.uint32(5000),
     numberOfParameters = cms.uint32(1),
     outputFile = cms.string('cmsgrid_final.lhe'),
