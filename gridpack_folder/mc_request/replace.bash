6aa4bf8eed370573fa7c38cb89734c55cc024783
Shin-Shan Yu <syu@ncu200.cern.ch>
2015-07-16 09:21:22 +0200

    add one more argument for replace.bash and add RS->ZZ->lephad

--- gridpack_folder/mc_request/replace.bash
+++ gridpack_folder/mc_request/replace.bash
@@ -1,13 +1,25 @@
 #!/bin/bash
 
+scriptname=`basename $0`
+EXPECTED_ARGS=3
+
+if [ $# -ne $EXPECTED_ARGS ]
+then
+    echo "Usage: $scriptname foldername Spin EOSDirVersion"
+    echo "Example: ./$scriptname RSGraviton_ZZ_ZlepZhad_narrow_M600 2 v1"
+    exit 1
+fi
+
+
 type=$1
 spin=$2
+version=$3
 process=${type%%_narrow*}
 echo $process
 if [[ ! -e $process ]]; then 
     mkdir $process
 fi
-location="\/cvmfs\/cms.cern.ch\/phys_generator\/gridpacks\/slc6_amd64_gcc481\/13TeV\/madgraph\/V5_2.2.2\/exo_diboson\/Spin-${spin}\/${process}\/narrow\/v1\/"${type}"_tarball.tar.xz"
+location="\/cvmfs\/cms.cern.ch\/phys_generator\/gridpacks\/slc6_amd64_gcc481\/13TeV\/madgraph\/V5_2.2.2\/exo_diboson\/Spin-${spin}\/${process}\/narrow\/${version}\/${type}_tarball.tar.xz"
 echo $location
 filename=${type}_13TeV-madgraph_cff.py
 echo $filename >> file.txt
