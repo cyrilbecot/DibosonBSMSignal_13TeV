9e98911d70f815573ea100332289705bf1f7c00c
Shin-Shan Yu <syu@ncu200.cern.ch>
2015-08-12 10:02:20 +0200

    add RS->hh->VVbb full leptonic LHE producers

---
+++ gridpack_folder/mc_request/LHEProducer/Spin-2/RSGraviton_hh_hVVhbb_fullLep/RSGraviton_hh_hVVhbb_fullLep_narrow_M2000_13TeV-madgraph_cff.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+# link to cards:
+# https://github.com/cms-sw/genproductions/tree/33fa3ce90437bb781f5362a12b6889548cde78ee/bin/MadGraph5_aMCatNLO/cards/production/13TeV/exo_diboson/Spin-2/RSGraviton_hh_hVVhbb_fullLep/RSGraviton_hh_hVVhbb_fullLep_narrow_M2000
+
+
+externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.2.2/exo_diboson/Spin-2/RSGraviton_hh_hVVhbb_fullLep/narrow/v1/RSGraviton_hh_hVVhbb_fullLep_narrow_M2000_tarball.tar.xz'),
+    nEvents = cms.untracked.uint32(5000),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
+)
