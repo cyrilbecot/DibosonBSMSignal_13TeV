40c42c21cb9757d1551a7b3214a60b3e5cdc38e2
Shin-Shan <syu@cern.ch>
2015-09-04 13:45:12 +0200

    add two spin-2 inclusive modes

---
+++ gridpack_folder/mc_request/LHEProducer/Spin-2/RSGraviton_hh_hVVhbb_inclusive/RSGraviton_hh_hVVhbb_inclusive_narrow_M1000_13TeV-madgraph_cff.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+
+# link to cards:
+# https://github.com/cms-sw/genproductions/tree/31b6e7510443b74e0f9aac870e4eb9ae30c19d65/bin/MadGraph5_aMCatNLO/cards/production/13TeV/exo_diboson/Spin-2/RSGraviton_hh_hVVhbb_inclusive/RSGraviton_hh_hVVhbb_inclusive_narrow_M1000
+
+
+externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    args = cms.vstring('/cvmfs/cms.cern.ch/phys_generator/gridpacks/slc6_amd64_gcc481/13TeV/madgraph/V5_2.2.2/exo_diboson/Spin-2/RSGraviton_hh_hVVhbb_inclusive/narrow/v1/RSGraviton_hh_hVVhbb_inclusive_narrow_M1000_tarball.tar.xz'),
+    nEvents = cms.untracked.uint32(5000),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
+)
